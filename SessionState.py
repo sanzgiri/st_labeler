9eed09d65a96899b7bb9c40fccb60334e6174c6a
Ashutosh Sanzgiri <sanzgiri@gmail.com>
2020-10-17 18:18:42 -0700

    Update SessionState.py

--- SessionState.py
+++ SessionState.py
@@ -15,7 +15,7 @@ result:
 >>> session_state.user_name
 'Mary'
 """
-import streamlit.ReportThread as ReportThread
+from streamlit.report_thread import ReportThread
 from streamlit.server.Server import Server
 
 
